7cad451ea7775f312d2306ef5bb7ce3e7e249dc6
mayuthombre <mayu.thombre@contino.io>
2022-09-09 20:00:30 +1000

    cleaned & formatted tf files

--- infrastructure/modules/ecs/variable.tf
+++ infrastructure/modules/ecs/variable.tf
@@ -13,7 +13,7 @@ variable "ecsTaskExecutionRole" {
 variable "tags" {
   type        = map(string)
   description = "Use tags to identify project resources"
-  default     = ""
+  default     = {}
 }
 
 variable "name" {
