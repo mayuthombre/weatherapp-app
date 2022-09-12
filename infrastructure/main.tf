ef64103bf513ba14c8a373c0ec032a2d3a9255a4
mayuthombre <mayu.thombre@contino.io>
2022-09-12 17:31:09 +1000

    change region from US to AP souteast

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,6 +1,6 @@
 #calling ecs module from weatherapp-infra repo
 module "ecr" {
-  source = "git::https://github.com/mayuthombre/weatherapp-infra.git//infrastructure/modules/ecr?ref=test"
+  source = "git::https://github.com/mayuthombre/weatherapp-infra.git//infrastructure/modules/ecr?ref=master"
 }
 
 #calling iam module from weatherapp-infra repo
