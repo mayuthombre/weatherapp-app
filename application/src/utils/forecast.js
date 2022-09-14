fd19a14f7d782a3a792a71cb8f72b66b478737a2
mayuthombre <mayu.thombre@contino.io>
2022-09-14 15:45:46 +1000

    change body text

--- application/src/utils/forecast.js
+++ application/src/utils/forecast.js
@@ -28,7 +28,7 @@ const forecast = (latitude, longtitude, callback) => {
             " °C. There is a " +
             body.currently.precipProbability * 100 +
             "% chance of rain." +
-            " The humidity presently is " +
+            " The humidity is " +
             body.currently.humidity * 100 +
             "%.",
           temperature: body.currently.temperature
