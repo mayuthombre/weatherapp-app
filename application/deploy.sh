08e7de98bf9db37709794627d15e9fa803d4bcc7
mayuthombre <mayu.thombre@contino.io>
2022-11-28 15:39:53 +1100

    moving to second trial after first successful build

--- application/deploy.sh
+++ application/deploy.sh
@@ -8,8 +8,8 @@ valblue=`cat valblue`
 if [ $valgreen -eq 0 ]
 then
     docker build --platform linux/amd64 -t ${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${GREEN_REPO_NAME}:latest .
-    aws ecr get-login-password --region $(AWS_REGION) | docker login --username AWS --password-stdin $(ACCOUNT_ID).dkr.ecr.$(AWS_REGION).amazonaws.com
-    docker push $(ACCOUNT_ID).dkr.ecr.$(AWS_REGION).amazonaws.com/$(GREEN_REPO_NAME):latest
+    aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
+    docker push ${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${GREEN_REPO_NAME}:latest
     aws ecs update-service --cluster ${CLUSTER_NAME} --service ${GREEN_SERVICE_NAME} --force-new-deployment --region ${AWS_REGION}
 elif [ $valblue -eq 0 ]
 then
